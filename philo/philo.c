aeaf1c0485413379464f337c5cbed17c15fc5bb8
Imrane Halim <ihalim@c3r3p2.1337.ma>
2025-01-01 16:04:16 +0100

    [ADD] Philosopher struct

--- philo/philo.c
+++ philo/philo.c
@@ -6,7 +6,7 @@
 /*   By: ihalim <ihalim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/01 10:27:39 by ihalim            #+#    #+#             */
-/*   Updated: 2025/01/01 11:30:28 by ihalim           ###   ########.fr       */
+/*   Updated: 2025/01/01 16:00:37 by ihalim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 int	main(int ac, char **av)
 {
+	t_data	data;
+
 	if (ac < 4 || ac > 5)
 	{
 		write(STDERR_FILENO,
@@ -24,5 +26,8 @@ int	main(int ac, char **av)
 	}
 	if (!are_valid_args(ac, av))
 		return (EXIT_FAILURE);
+	data.num_of_philos = ft_atoi(av[1]);
+	if (data.num_of_philos < 1)
+		return (EXIT_SUCCESS);
 	return (0);
 }
--- philo/philo.h
+++ philo/philo.h
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
-/*                                                                            */
+/*	                                                                        */
 /*                                                        :::      ::::::::   */
 /*   philo.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ihalim <ihalim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/01 10:38:41 by ihalim            #+#    #+#             */
-/*   Updated: 2025/01/01 11:36:53 by ihalim           ###   ########.fr       */
+/*   Updated: 2025/01/01 15:54:24 by ihalim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,23 @@
 # include <pthread.h>
 # include <unistd.h>
 # include <limits.h>
+# include <sys/time.h>
 
+typedef struct s_philo
+{
+	int	time_to_die;
+	int	time_to_eat;
+	int	time_to_sleep;
+	int	eat_times;
+}	t_philo;
+
+typedef struct s_data
+{
+	int		num_of_philos;
+	t_philo	*philos;
+}	t_data;
+
+int	ft_atoi(char *s);
 int	are_valid_args(int ac, char **av);
 
 #endif
