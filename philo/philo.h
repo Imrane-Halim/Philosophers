f47e946cce634df2785d66578f9a3ded76ae6b1a
Imrane Halim <ihalim@c3r8p9.1337.ma>
2025-01-01 11:37:32 +0100

    [ADD] include guards

--- philo/philo.h
+++ philo/philo.h
@@ -6,14 +6,19 @@
 /*   By: ihalim <ihalim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/01 10:38:41 by ihalim            #+#    #+#             */
-/*   Updated: 2025/01/01 11:29:46 by ihalim           ###   ########.fr       */
+/*   Updated: 2025/01/01 11:36:53 by ihalim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <pthread.h>
-#include <unistd.h>
-#include <limits.h>
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <pthread.h>
+# include <unistd.h>
+# include <limits.h>
 
 int	are_valid_args(int ac, char **av);
+
+#endif
