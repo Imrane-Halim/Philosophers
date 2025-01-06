72a7fa0d340d6338206a55df14fdd6ce6f7a6006
Imrane-Halim <ihalim.professional@gmai.com>
2025-01-06 18:59:17 +0000

    added num of philos check

--- philo/philo.c
+++ philo/philo.c
@@ -6,7 +6,7 @@
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/01 10:27:39 by ihalim            #+#    #+#             */
-/*   Updated: 2025/01/06 17:52:55 by marvin           ###   ########.fr       */
+/*   Updated: 2025/01/06 18:58:23 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,12 @@ int	main(int ac, char **av)
 	}
 	if (!are_valid_args(ac, av))
 		return (EXIT_FAILURE);
+	if (ft_atoi(av[1]) == 0)
+	{
+		write(STDERR_FILENO, 
+			"You need at least 1 philosopher in the table!\n", 47);
+		return (EXIT_FAILURE);
+	}
 	data = init_struct(ac, av);
 	if (!data.philos)
 		return (write(STDERR_FILENO, "malloc error\n", 14), EXIT_FAILURE);
