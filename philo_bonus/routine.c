b8a079387fb41bad7a1eddfa5fd63faf5f17a7e4
Imrane Halim <ihalim@c3r8p2.1337.ma>
2025-01-16 13:57:40 +0100

    refactored philo_eat and fixed some norm errors

--- philo_bonus/routine.c
+++ philo_bonus/routine.c
@@ -6,55 +6,37 @@
 /*   By: ihalim <ihalim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/06 17:53:00 by marvin            #+#    #+#             */
-/*   Updated: 2025/01/16 13:44:32 by ihalim           ###   ########.fr       */
+/*   Updated: 2025/01/16 13:54:47 by ihalim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-void    philo_eat(t_data *data, t_philo *philo)
+void	philo_eat(t_data *data, t_philo *philo)
 {
-    sem_t   *print;
-    sem_t   *forks;
-    sem_t   *waiter;
-    sem_t   *death;
+	sem_t	*print;
+	sem_t	*forks;
+	sem_t	*waiter;
 
-    print = sem_open("print_sem", 0);
-    forks = sem_open("forks_sem", 0);
-    waiter = sem_open("waiter_sem", 0);
-    death = sem_open("death_sem", 0);
-    
-    sem_wait(waiter);  // Only half of philosophers can try to eat at once
-    
-    sem_wait(death);   // Protect last_meal_time access
-    sem_wait(print);
-    if (get_time_elapsed(philo->last_meal_time) > data->time_to_die)
-    {
-        sem_post(print);
-        sem_post(death);
-        sem_post(waiter);
-        return;
-    }
-    sem_post(print);
-    sem_post(death);
-    
-    sem_wait(forks);
-    
-    sem_wait(forks);
-    sem_wait(death);
-    sem_wait(print);
-    print_action(get_time_elapsed(data->start_time), philo->philo_num, TOOK_FORK);
-    print_action(get_time_elapsed(data->start_time), philo->philo_num, EAT);
-    philo->last_meal_time = get_current_time();
-    philo->eat_count++;
-    sem_post(print);
-    sem_post(death);
-    
-    ft_mssleep(data->time_to_eat);
-    sem_post(forks);
-    sem_post(forks);
-    sem_post(waiter);
-    philo->next_state = SLEEP;
+	print = sem_open("print_sem", 0);
+	forks = sem_open("forks_sem", 0);
+	waiter = sem_open("waiter_sem", 0);
+	sem_wait(waiter);
+	sem_wait(forks);
+	sem_wait(forks);
+	sem_wait(print);
+	print_action(get_time_elapsed(data->start_time),
+		philo->philo_num, TOOK_FORK);
+	print_action(get_time_elapsed(data->start_time),
+		philo->philo_num, EAT);
+	philo->last_meal_time = get_current_time();
+	philo->eat_count++;
+	sem_post(print);
+	ft_mssleep(data->time_to_eat);
+	sem_post(forks);
+	sem_post(forks);
+	sem_post(waiter);
+	philo->next_state = SLEEP;
 }
 
 void	philo_sleep(t_data *data, t_philo *philo)
@@ -92,8 +74,8 @@ void	routine(t_data *data, t_philo *philo)
 	{
 		if (philo->next_state == EAT && data->num_of_philos > 1)
 		{
-			if (philo->eat_count >= data->meals_count && data->meals_count
-				!= -1)
+			if (philo->eat_count >= data->meals_count
+				&& data->meals_count != -1)
 				break ;
 			philo_eat(data, philo);
 		}
@@ -102,7 +84,6 @@ void	routine(t_data *data, t_philo *philo)
 		else if (philo->next_state == THINK)
 			philo_think(data, philo);
 	}
-	exit(1);
 }
 
 void	run_simulation(t_data *data)
@@ -126,11 +107,10 @@ void	run_simulation(t_data *data)
 	dead_pid = waitpid(-1, &status, 0);
 	if (WEXITSTATUS(status) != 1)
 		return ;
-	i = 0;
-	while (i < data->num_of_philos)
+	i = -1;
+	while (++i < data->num_of_philos)
 	{
 		if (data->philos[i].pid != dead_pid)
 			kill(data->philos[i].pid, SIGKILL);
-		i++;
 	}
 }
